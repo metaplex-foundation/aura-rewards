cc2ca49bb0282a188a75112f0276cc5ea8dddb30
Kyrylo Stepanov <k.stepanov@tutanota.com>
2024-06-17 18:15:42 +0300

    add more tests for withdraw

--- programs/rewards/tests/rewards/withdraw_mining.rs
+++ programs/rewards/tests/rewards/withdraw_mining.rs
@@ -4,6 +4,7 @@ use mplx_rewards::utils::LockupPeriod;
 use solana_program::program_pack::Pack;
 use solana_program::pubkey::Pubkey;
 use solana_program_test::*;
+use solana_sdk::clock::SECONDS_PER_DAY;
 use solana_sdk::signature::Keypair;
 use solana_sdk::signer::Signer;
 use std::borrow::Borrow;
@@ -58,4 +59,31 @@ async fn success() {
     let mining_account = get_account(&mut context, &mining).await;
     let mining = Mining::unpack(mining_account.data.borrow()).unwrap();
     assert_eq!(mining.share, 170);
+}
+
+#[tokio::test]
+async fn success_with_5kk() {
+    let (mut context, test_rewards, user, mining) = setup().await;
+
+    let lockup_period = LockupPeriod::ThreeMonths;
+    test_rewards
+        .deposit_mining(&mut context, &mining, 5000000, lockup_period, &user)
+        .await
+        .unwrap();
+
+    advance_clock_by_ts(&mut context, (100 * SECONDS_PER_DAY).try_into().unwrap()).await;
+
+    test_rewards
+        .withdraw_mining(&mut context, &mining, 5000000, &user)
+        .await
+        .unwrap();
+
+    let reward_pool_account = get_account(&mut context, &test_rewards.mining_reward_pool).await;
+    let reward_pool = RewardPool::unpack(reward_pool_account.data.borrow()).unwrap();
+
+    assert_eq!(reward_pool.total_share, 0);
+
+    // let mining_account = get_account(&mut context, &mining).await;
+    // let mining = Mining::unpack(mining_account.data.borrow()).unwrap();
+    // assert_eq!(mining.share, 0);
 }
