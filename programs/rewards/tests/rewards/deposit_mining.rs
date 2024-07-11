70a7bb8ab111ffca754d2e21168f321d99923e6e
Kyrylo Stepanov <k.stepanov@tutanota.com>
2024-07-11 18:34:51 +0300

    Add tests for delegated staking

--- programs/rewards/tests/rewards/deposit_mining.rs
+++ programs/rewards/tests/rewards/deposit_mining.rs
@@ -88,3 +88,54 @@ async fn success_with_flex() {
     let mining = Mining::unpack(mining_account.data.borrow()).unwrap();
     assert_eq!(mining.share, 100);
 }
+
+#[tokio::test]
+async fn delegating_success() {
+    let (mut context, test_rewards, user, mining) = setup().await;
+
+    let delegate = Keypair::new();
+    let delegate_mining = test_rewards
+        .initialize_mining(&mut context, &delegate.pubkey())
+        .await;
+    test_rewards
+        .deposit_mining(
+            &mut context,
+            &delegate_mining,
+            3_000_000, // 18_000_000 of weighted stake
+            LockupPeriod::OneYear,
+            &delegate.pubkey(),
+            &delegate_mining,
+        )
+        .await
+        .unwrap();
+    let delegate_mining_account = get_account(&mut context, &delegate_mining).await;
+    let d_mining = Mining::unpack(delegate_mining_account.data.borrow()).unwrap();
+    assert_eq!(d_mining.share, 18_000_000);
+    assert_eq!(d_mining.stake_from_others, 0);
+
+    test_rewards
+        .deposit_mining(
+            &mut context,
+            &mining,
+            100,
+            LockupPeriod::Flex,
+            &user,
+            &delegate_mining,
+        )
+        .await
+        .unwrap();
+
+    let delegate_mining_account = get_account(&mut context, &delegate_mining).await;
+    let d_mining = Mining::unpack(delegate_mining_account.data.borrow()).unwrap();
+    assert_eq!(d_mining.share, 18_000_000);
+    assert_eq!(d_mining.stake_from_others, 100);
+
+    let reward_pool_account = get_account(&mut context, &test_rewards.reward_pool).await;
+    let reward_pool = RewardPool::unpack(reward_pool_account.data.borrow()).unwrap();
+
+    assert_eq!(reward_pool.total_share, 18_000_200);
+
+    let mining_account = get_account(&mut context, &mining).await;
+    let mining = Mining::unpack(mining_account.data.borrow()).unwrap();
+    assert_eq!(mining.share, 100);
+}
