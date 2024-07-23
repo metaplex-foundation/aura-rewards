989d5a39b93d4d4083bb90579ac58455d6466fea
Kyrylo Stepanov <k.stepanov@tutanota.com>
2024-07-23 17:07:35 +0300

    update client version

--- clients/js/test/_setup.ts
+++ clients/js/test/_setup.ts
@@ -1,6 +1,6 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import { createUmi as basecreateUmi } from '@metaplex-foundation/umi-bundle-tests';
-import { mplProjectName } from '../src';
+// /* eslint-disable import/no-extraneous-dependencies */
+// import { createUmi as basecreateUmi } from '@metaplex-foundation/umi-bundle-tests';
+// import { mplProjectName } from '../src';
 
-export const createUmi = async () =>
-  (await basecreateUmi()).use(mplProjectName());
+// export const createUmi = async () =>
+//   (await basecreateUmi()).use(mplProjectName());
