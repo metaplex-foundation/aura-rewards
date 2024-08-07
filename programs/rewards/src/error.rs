872b330c156a06bde2b67800c0b62f95e45de3b5
Kyrylo Stepanov <k.stepanov@tutanota.com>
2024-08-07 13:52:43 +0300

    remove redundant error

--- programs/rewards/src/error.rs
+++ programs/rewards/src/error.rs
@@ -80,11 +80,6 @@ pub enum MplxRewardsError {
     /// Account is already initialized
     #[error("Account is already initialized")]
     AlreadyInitialized,
-
-    /// 19
-    /// No keys smaller than given
-    #[error("No keys smaller than given")]
-    NoKeysSmallerThanGiven,
 }
 
 impl PrintProgramError for MplxRewardsError {
