406078fcc3656dfeb57ae776ee81097480a27a62
Kyrylo Stepanov <k.stepanov@tutanota.com>
2024-08-07 14:06:36 +0300

    ad-hoc fix for client generation

--- clients/js/bench/_setup.ts
+++ clients/js/bench/_setup.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { createUmi as basecreateUmi } from '@metaplex-foundation/umi-bundle-tests';
 import {
-  mplProjectName,
+  Rewards,
 } from '../src';
 
-export const createUmi = async () => (await basecreateUmi()).use(mplProjectName());
+export const createUmi = async () => (await basecreateUmi()).use(Rewards());
