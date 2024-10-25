5e0f08b2d934411967067225ace7a3defc677a0d
Kyrylo Stepanov <k.stepanov@tutanota.com>
2024-10-25 14:33:13 +0300

    forbid all calls not from the staking contract

--- programs/rewards/src/entrypoint.rs
+++ programs/rewards/src/entrypoint.rs
@@ -2,6 +2,8 @@
 use crate::{error::MplxRewardsError, instructions::process_instruction};
 
 #[cfg(not(feature = "testing"))]
+use crate::STAKING_PROGRAM_ID;
+#[cfg(not(feature = "testing"))]
 use solana_program::instruction::get_stack_height;
 use solana_program::{
     account_info::AccountInfo, entrypoint, entrypoint::ProgramResult,
@@ -18,7 +20,7 @@ fn program_entrypoint<'a>(
     instruction_data: &[u8],
 ) -> ProgramResult {
     #[cfg(not(feature = "testing"))]
-    if get_stack_height() == TRANSACTION_LEVEL_STACK_HEIGHT {
+    if program_id != &STAKING_PROGRAM_ID || get_stack_height() == TRANSACTION_LEVEL_STACK_HEIGHT {
         return Err(MplxRewardsError::ForbiddenInvocation.into());
     }
 
--- programs/rewards/src/lib.rs
+++ programs/rewards/src/lib.rs
@@ -11,6 +11,10 @@ pub mod instructions;
 pub mod state;
 pub mod utils;
 
-pub use solana_program;
+pub use solana_program::{self, pubkey::Pubkey};
 
 solana_program::declare_id!("BF5PatmRTQDgEKoXR7iHRbkibEEi83nVM38cUKWzQcTR");
+pub const STAKING_PROGRAM_ID: Pubkey = Pubkey::new_from_array([
+    164, 185, 242, 100, 167, 7, 245, 144, 85, 224, 196, 151, 192, 113, 216, 27, 46, 144, 114, 107,
+    253, 180, 250, 83, 45, 239, 58, 211, 199, 87, 250, 145,
+]);
